7eb23950fa26a67d0788eeb714c4869d09c07df3
Leandro Rangel <rangel94@outlook.com>
2023-06-22 10:30:36 -0300

    config.base

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -19,7 +19,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/ignite-design-system/'
     }
 
     return config
